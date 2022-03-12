fad9160fc851052fa32ac84d2b2578c1facced6e
kennykded <82447341+kennykded@users.noreply.github.com>
2022-03-12 01:27:45 +0100

    print tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Print alphabets in reverse
+* Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
