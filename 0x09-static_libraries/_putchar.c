e51bb60aaaf92bacaa1e0ebe9c145fc13156a5f9
kennykded <82447341+kennykded@users.noreply.github.com>
2022-04-02 10:10:07 +0100

    static libraries

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,39 +1,13 @@
-#include "main.h"
+#include <unistd.h>
+
 /**
-* sqtRecursive - computes square root recursively
-* @n: given number
-* @m: comparison number
-* Return: 1 if not found sqrroot, else sqrroot
-**/
-int sqtRecursive(int n, int m)
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-    if (n <= 0)
-        return (-1);
-    if (n * n == m)
-        return (n);
-    return (sqtRecursive(n - 1, m));
-}
-/**
-* _sqrt_recursion - finds the natural square root of a number
-* @n: given number
-* Return: square root of n or -1
-**/
-int _sqrt_recursion(int n)
-{
-    if (n == 1)
-        return (1);
-    return (sqtRecursive(n / 2, n));
-}
-/**
-* is_prime_number - checks if a given number is prime
-* @n: given number
-* Return: 1 if number is prime else 0
-**/
-int is_prime_number(int n)
-{
-    if (n <= 1 || _sqrt_recursion(n) >= 1)
-        return (0);
-    if (_sqrt_recursion(n) == -1)
-        return (1);
-    return (_sqrt_recursion(n));
+	return (write(1, &c, 1));
 }
