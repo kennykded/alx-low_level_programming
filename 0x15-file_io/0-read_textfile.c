21c0160be6357615d5e4137b0662c2463e20f12a
kennykded <82447341+kennykded@users.noreply.github.com>
2022-05-03 16:42:39 +0100

    FILE/IO

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,14 +11,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int i, y;
 	char *buf;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
+
 	fd = open(filename, O_RDONLY);
+
 	if (fd < 0)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
 	if (!buf)
 		return (0);
+
 	i = read(fd, buf, letters);
 	if (i < 0)
 	{
